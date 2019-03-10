188f90c63ef3f5002fdb77fdd00c24df50ca49f4
Jasneet Bhatti <jabhatti@microsoft.com>
2019-03-10 03:29:20 -0700

    Updated datasets for assignment 3

--- assignment3/run_experiment.py
+++ assignment3/run_experiment.py
@@ -88,19 +88,33 @@ if __name__ == '__main__':
 
     datasets = []
     dataset1_details = {
-            'data': loader.StatlogVehicleData(verbose=verbose, seed=seed),
-            'name': 'statlog_vehicle',
-            'readable_name': 'Statlog Vehicle',
-            'best_nn_params': {'NN__activation': ['relu'], 'NN__alpha': [1.0],
-                               'NN__hidden_layer_sizes': [(36, 36)], 'NN__learning_rate_init': [0.016]}
+            'data': loader.AdultData(verbose=verbose, seed=seed),
+            'name': 'adult',
+            'readable_name': 'Adult',
+            'best_nn_params': {'NN__activation': ['logistic'], 'NN__alpha': [0.00031622776601683794],
+                               'NN__hidden_layer_sizes': [(53, 53, 53)], 'NN__learning_rate_init': [0.016]}
         }
     dataset2_details = {
-            'data': loader.HTRU2Data(verbose=verbose, seed=seed),
-            'name': 'htru2',
-            'readable_name': 'HTRU2',
-            'best_nn_params': {'NN__activation': ['relu'], 'NN__alpha': [1.0],
-                               'NN__hidden_layer_sizes': [(36, 36)], 'NN__learning_rate_init': [0.016]}
+            'data': loader.MadelonData(verbose=verbose, seed=seed),
+            'name': 'madelon',
+            'readable_name': 'Madelon',
+            'best_nn_params': {'NN__activation': ['relu'], 'NN__alpha': [10.0],
+                               'NN__hidden_layer_sizes': [(1000, 1000)], 'NN__learning_rate_init': [0.032]}
     }
+    # dataset1_details = {
+    #         'data': loader.StatlogVehicleData(verbose=verbose, seed=seed),
+    #         'name': 'statlog_vehicle',
+    #         'readable_name': 'Statlog Vehicle',
+    #         'best_nn_params': {'NN__activation': ['relu'], 'NN__alpha': [1.0],
+    #                            'NN__hidden_layer_sizes': [(36, 36)], 'NN__learning_rate_init': [0.016]}
+    #     }
+    # dataset2_details = {
+    #         'data': loader.HTRU2Data(verbose=verbose, seed=seed),
+    #         'name': 'htru2',
+    #         'readable_name': 'HTRU2',
+    #         'best_nn_params': {'NN__activation': ['relu'], 'NN__alpha': [1.0],
+    #                            'NN__hidden_layer_sizes': [(36, 36)], 'NN__learning_rate_init': [0.016]}
+    # }
     if args.dataset1:
         datasets.append(dataset1_details)
     elif args.dataset2:
