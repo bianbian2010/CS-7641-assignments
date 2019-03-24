629a71891f93e4b8234b2f233700c882f36a8fbb
Jasneet Bhatti <jabhatti@microsoft.com>
2019-03-23 20:05:06 -0700

    Added ICA run with dim = 60

--- assignment3/run_clustering.ps1
+++ assignment3/run_clustering.ps1
@@ -3,8 +3,8 @@
 # Replace 'X' below with the optimal values found
 # If you want to first generate data and updated datasets, remove the "--skiprerun" flags below
 
-python run_experiment.py --ica --dataset1 --dim 45 --skiprerun --verbose --threads -1 --seed 13 | tee ica-dataset1-clustering.log 2>&1
-python run_experiment.py --ica --dataset2 --dim 30 --skiprerun --verbose --threads -1 --seed 13 | tee ica-dataset2-clustering.log 2>&1
+python run_experiment.py --ica --dataset1 --dim 60 --skiprerun --verbose --threads -1 --seed 13 | tee ica-dataset1-clustering.log 2>&1
+python run_experiment.py --ica --dataset2 --dim 60 --skiprerun --verbose --threads -1 --seed 13 | tee ica-dataset2-clustering.log 2>&1
 # python run_experiment.py --pca --dataset1 --dim 10 --skiprerun --verbose --threads -1 --seed 13 | tee pca-dataset1-clustering.log 2>&1
 # python run_experiment.py --pca --dataset2 --dim 12 --skiprerun --verbose --threads -1 --seed 13 | tee pca-dataset2-clustering.log 2>&1
 # python run_experiment.py --rp  --dataset1 --dim 8  --skiprerun --verbose --threads -1 --seed 1113 | tee rp-dataset1-clustering.log  2>&1
