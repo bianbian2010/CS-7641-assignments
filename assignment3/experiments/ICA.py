a1760e6eb566723db17c4df38c1da758fc6f8d40
Jasneet Bhatti <jabhatti@microsoft.com>
2019-03-24 09:04:18 -0700

    Uncommenting code in ICA

--- assignment3/experiments/ICA.py
+++ assignment3/experiments/ICA.py
@@ -36,16 +36,16 @@ class ICAExperiment(experiments.BaseExperiment):
 
         # %% Data for 1
         ica = FastICA(random_state=self._details.seed)
-        # kurt = {}
-        # for dim in self._dims:
-        #     ica.set_params(n_components=dim)
-        #     tmp = ica.fit_transform(self._details.ds.training_x)
-        #     tmp = pd.DataFrame(tmp)
-        #     tmp = tmp.kurt(axis=0)
-        #     kurt[dim] = tmp.abs().mean()
+        kurt = {}
+        for dim in self._dims:
+            ica.set_params(n_components=dim)
+            tmp = ica.fit_transform(self._details.ds.training_x)
+            tmp = pd.DataFrame(tmp)
+            tmp = tmp.kurt(axis=0)
+            kurt[dim] = tmp.abs().mean()
 
-        # kurt = pd.Series(kurt)
-        # kurt.to_csv(self._out.format('{}_scree.csv'.format(self._details.ds_name)))
+        kurt = pd.Series(kurt)
+        kurt.to_csv(self._out.format('{}_scree.csv'.format(self._details.ds_name)))
 
         # Calculate reconstruction error
         reconstruction_error = defaultdict(dict)
@@ -59,16 +59,16 @@ class ICAExperiment(experiments.BaseExperiment):
         t.to_csv(self._out.format('{}_scree2.csv'.format(self._details.ds_name)))
 
         # # %% Data for 2
-        # grid = {'ica__n_components': self._dims, 'NN__alpha': self._nn_reg, 'NN__hidden_layer_sizes': self._nn_arch}
-        # ica = FastICA(random_state=self._details.seed)
-        # mlp = MLPClassifier(activation='relu', max_iter=2000, early_stopping=True, random_state=self._details.seed)
-        # pipe = Pipeline([('ica', ica), ('NN', mlp)], memory=experiments.pipeline_memory)
-        # gs, final_estimator = self.gs_with_best_estimator(pipe, grid)
-        # self.log("Grid search complete")
+        grid = {'ica__n_components': self._dims, 'NN__alpha': self._nn_reg, 'NN__hidden_layer_sizes': self._nn_arch}
+        ica = FastICA(random_state=self._details.seed)
+        mlp = MLPClassifier(activation='relu', max_iter=2000, early_stopping=True, random_state=self._details.seed)
+        pipe = Pipeline([('ica', ica), ('NN', mlp)], memory=experiments.pipeline_memory)
+        gs, final_estimator = self.gs_with_best_estimator(pipe, grid)
+        self.log("Grid search complete")
 
-        # tmp = pd.DataFrame(gs.cv_results_)
-        # tmp.to_csv(self._out.format('{}_dim_red.csv'.format(self._details.ds_name)))
-        # self.log("Done")
+        tmp = pd.DataFrame(gs.cv_results_)
+        tmp.to_csv(self._out.format('{}_dim_red.csv'.format(self._details.ds_name)))
+        self.log("Done")
 
     def perform_cluster(self, dim_param):
         self.log('Running clustering for {} with dim param {}'.format(self.experiment_name(), dim_param))
